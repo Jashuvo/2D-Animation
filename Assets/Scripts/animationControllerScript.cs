f913b9b7633264f3d615b126b21bae180c9e2e61
Jubayed Ahmed <40147968+Jashuvo@users.noreply.github.com>
2019-11-21 12:19:27 +0600

    kjkhbjk

---
+++ Assets/Scripts/animationControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationControllerScript : MonoBehaviour
+{
+    public Animator animationController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animationController = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animationController.SetBool("swing", true);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            animationController.SetBool("swing", false);
+        }
+        float playerWalk = Input.GetAxis("Horizontal");
+        animationController.SetFloat("walk", playerWalk);
+    }
+}
